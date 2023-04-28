8e045c6a493431881864c480e8907ab9171f8239
Niyi Adetunji <adenadet01@gmail.com>
2023-04-28 16:39:39 +0100

    Dashboard s added

--- resources/views/partials/main/left.blade.php
+++ resources/views/partials/main/left.blade.php
@@ -32,7 +32,6 @@
                         </li>
                     </ul>
                 </li>
-                <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon fas fa-th"></i><p>Simple Link</p></a></li>
             </ul>
         </nav>
     </div>
